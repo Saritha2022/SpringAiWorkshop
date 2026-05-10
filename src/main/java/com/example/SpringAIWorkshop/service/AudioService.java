42266043eb3747c908739d1cdcee83ea8b3f81e1
Saritha2022 <101663993+Saritha2022@users.noreply.github.com>
2026-05-10 19:58:55 +0530

    uploaded from local

---
+++ src/main/java/com/example/SpringAIWorkshop/service/AudioService.java
@@ -0,0 +1,44 @@
+package com.example.SpringAIWorkshop.service;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AudioService {
+	
+	OpenAiAudioSpeechModel audioModel;
+	public AudioService(OpenAiAudioSpeechModel audioModel) {
+		this.audioModel = audioModel;
+	}
+	
+	
+	public String convertTextToSpeech(String text) {
+		
+		OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+			    .model("gpt-4o-mini-tts")
+			    .voice(OpenAiAudioApi.SpeechRequest.Voice.BALLAD)
+			    .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+			    .speed(1.0) //double in latest code
+			    .build();
+		
+		TextToSpeechPrompt speechPrompt = new TextToSpeechPrompt(text, speechOptions);
+		TextToSpeechResponse response = audioModel.call(speechPrompt);
+		
+		byte[] audio = response.getResult().getOutput();
+		
+		try (FileOutputStream f = new FileOutputStream("May08Audio.mp3")) {
+			f.write(audio);
+			System.out.println("Audio file saved as output.mp3");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "Audio generation successful. Check mp3 file.";
+	}
+}
---
+++ src/main/java/com/example/SpringAIWorkshop/service/ChatService.java
@@ -0,0 +1,34 @@
+package com.example.SpringAIWorkshop.service;
+
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+	//1. Field injection
+	// @Autowired
+	// private ChatModel chatModel;
+
+	//2. Constructor injection (recommended)
+	private final ChatModel chatModel;
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String askAI(String prompt) {
+    	
+    	OpenAiChatOptions options = OpenAiChatOptions.builder()
+                .model("gpt-5.1")
+                .maxCompletionTokens(500)
+                .temperature(0.5)
+                .build();
+    	
+        ChatResponse response = chatModel.call(new Prompt(prompt, options));
+      
+        return response.getResult().getOutput().getText();
+    }
+}
